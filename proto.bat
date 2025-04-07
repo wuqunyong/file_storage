1d5aef6813206f1fb2f1258ee93cc4b1d898c728
wuqunyong <277292765@qq.com>
2025-04-07 10:32:51 +0800

    no message

---
+++ proto.bat
@@ -0,0 +1,20 @@
+@REM # 生成PB文件
+@REM - protoc.exe --plugin=protoc-gen-go=%BIN_DIR%\protoc-gen-go.exe --go_out=. .\protobuf\pbtest.proto
+@REM - protoc.exe --plugin=protoc-gen-doc=%BIN_DIR%\protoc-gen-doc.exe --doc_out=.\protobuf .\protobuf\pbtest.proto
+@REM E:\ProtobufBin\protoc.exe -I=protobuf --plugin=protoc-gen-go=E:\ProtobufBin\protoc-gen-go.exe  --go_out=. login_msg.proto
+
+
+
+
+
+
+E:\ProtobufBin\protoc.exe -I=protobuf --plugin=protoc-gen-go=E:\ProtobufBin\protoc-gen-go.exe  --go_out=.   --go_opt=paths=source_relative --proto_path=. rpc_msg/rpc_msg.proto
+@REM E:\ProtobufBin\protoc.exe -I=protobuf --plugin=protoc-gen-go=E:\ProtobufBin\protoc-gen-go.exe  --go_out=.   --go_opt=paths=source_relative --proto_path=. nats_msg/nats_msg.proto
+
+
+@REM E:\ProtobufBin\protoc.exe -I=protobuf --plugin=protoc-gen-go-vtproto=E:\ProtobufBin\protoc-gen-go-vtproto.exe  --go-vtproto_out=.    rpc_msg/rpc_msg.proto
+@REM E:\ProtobufBin\protoc.exe -I=protobuf --plugin=protoc-gen-go-vtproto=E:\ProtobufBin\protoc-gen-go-vtproto.exe  --go-vtproto_out=.    nats_msg/nats_msg.proto
+
+
+@REM E:\ProtobufBin\protoc-go-inject-tag.exe -input=C:/Users/Administrator/Desktop/编程杂记/file_storage/rpc_msg/rpc_msg.pb.go
+@REM E:\ProtobufBin\protoc-go-inject-tag.exe -input=C:/Users/Administrator/Desktop/编程杂记/file_storage/protobuf/pbtest.pb.go
